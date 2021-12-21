1ea4530ea91f260a9ff27927fdeef1735c7b086f
BuildabilityResearcher <anonymus.developer2019@gmail.com>
2021-12-21 18:48:44 +0000

    Fix results for intemediate snapshots

--- scripts/runNotebooks.sh
+++ scripts/runNotebooks.sh
@@ -1,1 +1,1 @@
-docker run -d --rm --name jupyter-bugs -p 8888:8888 -v $PWD:/home/ -w /home/ jupyter-bugs
+docker run -d --rm --name jupyter-bugs -p 8888:8888 -v $PWD:/home/ -w /home/ jupyter-bugs:emse
